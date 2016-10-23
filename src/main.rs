90cb00b10ea9553ab72c59dfa11afce4b4618709
Daniel Collin <daniel@collin.com>
2016-10-23 10:44:29 +0200

    Some more work on connection

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,9 @@
 use std::os::raw::{c_void, c_int, c_uchar};
 use std::mem::transmute;
 
-struct MyTool {
-    button: PushButton,
-}
-
-impl MyTool {
-    fn new() -> Box<MyTool> {
-        Box::new(MyTool {
-            button: PushButton::new(),
-        })
-    }
-
-    fn button_callback(&mut self) {
-        println!("button callback");
-    }
-
-    fn some_init_code(&self) {
-    }
-}
-
 #[macro_export]
 macro_rules! connect_no_args {
-    ($p_name:expr, $data:expr, $call_type:ident, $callback:path) => {
+    ($sender:expr, $signal:expr, $data:expr, $call_type:ident, $callback:path) => {
         {
             extern "C" fn temp_call(target: *mut c_void) {
                 unsafe {
@@ -36,10 +17,32 @@ macro_rules! connect_no_args {
             }
 
             unsafe {
-                let object = (*(*$p_name.widget).base).o;
-                connect(object, $data, temp_call);
+                let object = (*(*$sender.widget).base).o;
+                connect(object, $signal, $data, temp_call);
             }
         }
+    }
+}
+
+struct MyTool {
+    button: PushButton,
+    some_value: i32,
+}
+
+impl MyTool {
+    fn new() -> Box<MyTool> {
+        Box::new(MyTool {
+            button: PushButton::new(),
+            some_value: 42,
+        })
+    }
+
+    fn button_callback(&mut self) {
+        self.some_value += 1;
+        println!("button callback, {}", self.some_value);
+    }
+
+    fn some_init_code(&self) {
     }
 }
 
@@ -47,11 +50,15 @@ macro_rules! connect_no_args {
 fn main() {
     let app = Application::new();
 
-    let tool = MyTool::new();
+    let mut tool = MyTool::new();
 
-    connect_no_args!(&tool.button, &tool, MyTool, MyTool::button_callback); 
+    connect_no_args!(&tool.button, push_button::RELEASED, &mut *tool, MyTool, MyTool::button_callback); 
 
     app.run();
+}
+
+pub mod push_button {
+    pub const RELEASED: &'static [u8] = b"2released()\0";
 }
 
 pub struct Application {
@@ -91,10 +98,10 @@ impl Application {
 }
 
 
-pub fn connect<D>(object: *const GUObject, data: &D, fun: extern fn(*mut c_void)) {
+
+pub fn connect<D>(object: *const GUObject, signal: &[u8], data: &D, fun: extern fn(*mut c_void)) {
     unsafe {
-        //let object = (*(*ctrl.button).base).o;
-        ((*object).connect)(object as *const c_void, b"dummy\0" as *const c_uchar, transmute(data), fun);
+        ((*object).connect)(object as *const c_void, signal.as_ptr(), transmute(data), fun);
     }
 }
 
